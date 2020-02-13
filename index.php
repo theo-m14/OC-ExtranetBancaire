85633f35dee940c111a863410d6a67f1e1ee00b4
T.Michel <theo.michel2@outlook.fr>
2020-02-13 22:16:12 +0100

    Réalisation de la partie présentations

--- index.php
+++ index.php
@@ -1,16 +1,37 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
 	<meta charset="UTF-8">
 	<title>Extranet GBAF</title>
 	<link rel="stylesheet" type="text/css" href="css/headercss.css">
 	<link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
+
 <body>
 	<?php include("header.php"); ?>
 	<div class="presentation">
-		<h1>Le BGAF et son Extranet</h1>
+		<h1>Le GBAF et son Extranet</h1>
+		<p>
+			Le Groupement Banque Assurance Français (GBAF) est une fédération
+			représentant les 6 grands groupes français :
+		</p>
+		<ul>
+			<li>BNP Paribas</li>
+			<li>BPCE</li>
+			<li>Crédit Agricole</li>
+			<li>Crédit Mutuel-CIC</li>
+			<li>Société Générale</li>
+			<li>La Banque Postale</li>
+		</ul>
+		<p>Ce site est destiné à regrouper des informations sur les différents acteurs et partenaires du secteur bancaire,
+			tels que les associations ou les financeurs solidaires.
+		</p>
 	</div>
-	
+	<div class="acteurs_partenaire">
+
+	</div>
+
 </body>
+
 </html>
