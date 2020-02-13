5a6132f3854b5e5af66151087a1b48d823d153be
T.Michel <theo.michel2@outlook.fr>
2020-02-13 19:36:06 +0100

    header-done présentation en cours

---
+++ header.php
@@ -0,0 +1,8 @@
+<div class="header">
+	<div class="divlogo_gbaf">
+		<img src="img/gbaf.png" class="logo_gbaf">
+	</div>
+	<div class="user_spec">
+		<p>First Name : Last Name </p>
+	</div>
+</div>
---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<title>Extranet GBAF</title>
+	<link rel="stylesheet" type="text/css" href="css/headercss.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+	<?php include("header.php"); ?>
+	<div class="presentation">
+		<h1>Le BGAF et son Extranet</h1>
+	</div>
+	
+</body>
+</html>
