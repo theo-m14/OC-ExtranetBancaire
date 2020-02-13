810d7bae1fac848f8de63cd096d6f042506f8be0
T.Michel <theo.michel2@outlook.fr>
2020-02-14 00:14:22 +0100

    Rajout formulaire de connexion (non fonctionnel)

--- index.php
+++ index.php
@@ -18,30 +18,53 @@ try {           //Récupération BDD
 
 <body>
 	<?php include("header.php");
+	if (isset($_POST['username']))  //Si on vient d'envoyer le login
+	{
+		//Vérification correspondance login / mdp
+		//if(id_user.mdp = post_mdp){ $_SESSION['username']=$_POST['username']}
+		//else{ "mdp incorrect" }
+	}
+
+	if (!isset($_SESSION['username']))  //Si aucune session n'est enregistré
+	{
 
 	?>
-	<div class="presentation">
-		<h1>Le GBAF et son Extranet</h1>
-		<p>
-			Le Groupement Banque Assurance Français (GBAF) est une fédération
-			représentant les 6 grands groupes français :
-		</p>
-		<ul>
-			<li>BNP Paribas</li>
-			<li>BPCE</li>
-			<li>Crédit Agricole</li>
-			<li>Crédit Mutuel-CIC</li>
-			<li>Société Générale</li>
-			<li>La Banque Postale</li>
-		</ul>
-		<p>Ce site est destiné à regrouper des informations sur les différents acteurs et partenaires du secteur bancaire,
-			tels que les associations ou les financeurs solidaires.
-		</p>
-	</div>
-	<div class="acteurs_partenaire">
+		<div class="form_login">
+			<form action="index.php" method="post">
+				<label for="identifiant">Login</label>
+				<input type="text" name="login" id="identifiant"></br><br>
+				<label for="password">Password</label>
+				<input type="password" name="pass" id="password"></br>
+				<input type="submit" value="Valider">
+		</div>
 
-	</div>
-	<?php include("footer.php") ?>
+	<?php
+	} else {  //Si la session existe on affiche le site
+	?>
+		<div class="presentation">
+			<h1>Le GBAF et son Extranet</h1>
+			<p>
+				Le Groupement Banque Assurance Français (GBAF) est une fédération
+				représentant les 6 grands groupes français :
+			</p>
+			<ul>
+				<li>BNP Paribas</li>
+				<li>BPCE</li>
+				<li>Crédit Agricole</li>
+				<li>Crédit Mutuel-CIC</li>
+				<li>Société Générale</li>
+				<li>La Banque Postale</li>
+			</ul>
+			<p>Ce site est destiné à regrouper des informations sur les différents acteurs et partenaires du secteur bancaire,
+				tels que les associations ou les financeurs solidaires.
+			</p>
+		</div>
+		<div class="acteurs_partenaire">
+
+		</div>
+
+	<?php }
+	include("footer.php") ?>
 </body>
 
 </html>
