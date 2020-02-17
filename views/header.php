fc343a59e565f24fc5e97c16e8e4e4e3207f2674
T.Michel <theo.michel2@outlook.fr>
2020-02-17 12:18:17 +0100

    Changement des chemins des fichiers

--- views/header.php
+++ views/header.php
@@ -1,6 +1,6 @@
 <div class="header">
 	<div class="divlogo_gbaf">
-		<img src="img/gbaf.png" class="logo_gbaf">
+		<img src="public/img/gbaf.png" class="logo_gbaf">
 	</div>
 	<div class="user_spec">
 		<?php if (isset($_SESSION['username'])) { ?>
